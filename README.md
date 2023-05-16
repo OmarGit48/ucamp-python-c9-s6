209f2a625718071dea74330b447630aad7463b3a
Omar Ontiveros <ontiveros2000@hotmail.com>
2023-05-16 02:39:37 -0600

    algo no entiendo de while fals ey while true, no estoy viendo algo

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ semana 6 sobre Iteraciones y ciclos (while, for y más)
 1. Ya haceindo repositorio sin ver videos ya hago clone, add, commit y push
 2. Con dificultad pero avanzo con el reto de la semana 6, muchos errores pero a prueba y error para ver que si puedo o como funciona
 3. ciclado
+4. batallo para saber donde termina el While, donde es true y donde es false
--- reto_semana6.py
+++ reto_semana6.py
@@ -2,17 +2,23 @@ import sys
 
 while True:
     contraseña1 = input("Ingrese una contraseña con un numero: ")
-    try:
-        contraseña1 = int(contraseña1[0])
-    except ValueError:
-        print("La contraseña debe comenzar con un número")
-        continue
-    for contraseña1[0] in contraseña1 :
-        print("ya")
-    if contraseña1[0] == int :
-            print("Ingrese la contraseña nuevamente: ")
+         
+    if contraseña1[0].isdigit() :
+        confirmacion = input("Ingrese la contraseña nuevamente: ")
+        if contraseña1 == confirmacion:
+            print("Contraseña establecida correctamente.")
+            break
         
+        else :
+            print ("Las contraseñas no coinciden")  
+              
+    else :
+        print("la contraseña debe empezar con un numero")    
+        
+print("Fin del programa")        
+
         #for contraseña1[0] in contraseña1 :
+        
             #print("Ingrese la contraseña nuevamente")
     
 #if contraseña1[0] == int :
