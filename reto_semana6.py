1d0eac8249c26ef9507f6f95763bf1ef50d40677
Omar Ontiveros <ontiveros2000@hotmail.com>
2023-05-24 22:20:15 -0600

    Atrasado, terminando apenas semana 7, ponendome al dia, sencillo lo dejo el Teacher Ariel, no habia tenio tiempo de ver el proyecto

--- reto_semana6.py
+++ reto_semana6.py
@@ -2,25 +2,26 @@ import sys
 
 while True:
     contraseña1 = input("Ingrese una contraseña con un numero: ")
-         
+       
     if contraseña1[0].isdigit() :
         confirmacion = input("Ingrese la contraseña nuevamente: ")
-        if contraseña1 == confirmacion:
-            print("Contraseña establecida correctamente.")
+        if contraseña1 != confirmacion:
+            print("Las contraseñas no coinciden.")
+        elif contraseña1 != confirmacion: 
+            print ('La contraseñas no coiciden saldremos del programa') 
             break
-        
         else :
-            print ("Las contraseñas no coinciden")  
-              
+            print ("Contraseña establecida correctamente.")  
+            
     else :
         print("la contraseña debe empezar con un numero")    
-        
-print("Fin del programa")        
+    
+print("Fin del programa")       
 
-        #for contraseña1[0] in contraseña1 :
+        # for contraseña1[0] in contraseña1 :
         
-            #print("Ingrese la contraseña nuevamente")
+        #     print("Ingrese la contraseña nuevamente")
     
 #if contraseña1[0] == int :
-# print("Ingrese la contraseña nuevamente")
+# print("Ingrese la contraseña nuevamente")Contraseña establecida correctamente.
     
